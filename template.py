3a2dd3b56bedd00d6abc7c96ac5e78a168913d32
sathyajain12 <sathyajain9@gmail.com>
2024-10-04 20:42:24 +0530

    updated a file

--- template.py
+++ template.py
@@ -18,7 +18,7 @@ list_of_files=[
     "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
-    "tests/unit/__init.py",
+    "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
